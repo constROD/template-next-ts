5ff12fceaa386caebb220b86d74639b855030aa0
constROD <rialcantara21@gmail.com>
2022-06-01 00:39:13 +0800

    chore: update theme default style

--- src/shared/theme/index.ts
+++ src/shared/theme/index.ts
@@ -21,18 +21,15 @@ export const theme = (): ITheme => ({ colors, screens });
 export const ThemeProvider = SThemeProvider;
 export const styled: ThemedStyledInterface<ITheme> = defaultStyled;
 export const GlobalStyle = createGlobalStyle`
-  html, body {
-    background: #FAFDFF;
+ html, body {
     padding: 0;
     margin: 0;
     height: 100%;
     width: 100%;
-    font-size: 14px;
-    font-weight: 400;
-    font-family: 'Roboto', sans-serif;
+    -webkit-tap-highlight-color: transparent;
   }
 
-  #root {
+  #__next {
     display: grid;
     height: 100%;
   }
