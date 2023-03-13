ef48f81252fb9f8a16ac72465b82495cd77cefe7
constROD <rialcantara21@gmail.com>
2023-03-13 21:22:27 +0800

    chore: update default asset domain and version and util

--- src/shared/constants/Common.ts
+++ src/shared/constants/Common.ts
@@ -1,6 +1,6 @@
-export const IMAGE_URL = 'https://mydomain.com';
+export const DEFAULT_ASSET_DOMAIN = '/assets';
 
 /**
  * Increment this version number when you update the production assets.
  */
-export const ASSET_VERSION = '1.0.0';
+export const DEFAULT_ASSET_VERSION = '1.0.0';
--- src/shared/utils/Common.ts
+++ src/shared/utils/Common.ts
@@ -1,4 +1,5 @@
 import { format } from 'date-fns';
+import { DEFAULT_ASSET_DOMAIN, DEFAULT_ASSET_VERSION } from 'shared/constants/Common';
 
 export const formatDate = (date: Date | null, desiredFormat?: string) => {
   if (!date) return '';
@@ -10,3 +11,13 @@ export const logger = ({ path, event, log }: { path: string; event: string; log:
   const date = format(new Date(), 'yyyy/MM/dd hh:mm:ss');
   console.log(`[${date}]: ${path} (${event}) >> `, JSON.stringify(log, null, 2));
 };
+
+export const makeImageUrl = ({
+  url,
+  domain,
+  version,
+}: {
+  url: string;
+  domain?: string;
+  version?: string;
+}) => `${domain ?? DEFAULT_ASSET_DOMAIN}${url}?v=${version ?? DEFAULT_ASSET_VERSION}`;
