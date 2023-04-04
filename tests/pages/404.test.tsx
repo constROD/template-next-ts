4657aa990185139bdd1cba20937898925df656f3
constROD <rialcantara21@gmail.com>
2023-04-05 06:16:03 +0800

    chore: update sample test

--- tests/pages/404.test.tsx
+++ tests/pages/404.test.tsx
@@ -1,11 +1,11 @@
-import { fireEvent, render } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { useRouter } from 'next/router';
 import NotFoundPage from 'pages/404';
 import { ROUTES } from 'shared/constants/commons';
 
 // Mock the useRouter hook so we can test its usage
 jest.mock('next/router', () => ({
-  useRouter: jest.fn() as jest.Mock,
+  useRouter: jest.fn(),
 }));
 
 describe('NotFoundPage', () => {
@@ -13,17 +13,17 @@ describe('NotFoundPage', () => {
     const pushMock = jest.fn();
     (useRouter as jest.Mock).mockReturnValue({ push: pushMock });
 
-    const { getByText } = render(<NotFoundPage />);
-    expect(getByText(/Sorry, the page you visited does not exist./i)).toBeInTheDocument();
-    expect(getByText(/Back Home/i)).toBeInTheDocument();
+    render(<NotFoundPage />);
+    expect(screen.getByText(/Sorry, the page you visited does not exist./i)).toBeInTheDocument();
+    expect(screen.getByText(/Back Home/i)).toBeInTheDocument();
   });
 
   it('should navigate to the home page when the button is clicked', () => {
     const pushMock = jest.fn();
     (useRouter as jest.Mock).mockReturnValue({ push: pushMock });
 
-    const { getByText } = render(<NotFoundPage />);
-    const button = getByText(/Back Home/i);
+    render(<NotFoundPage />);
+    const button = screen.getByText(/Back Home/i);
     fireEvent.click(button);
 
     expect(pushMock).toHaveBeenCalledWith(ROUTES.HOME);
