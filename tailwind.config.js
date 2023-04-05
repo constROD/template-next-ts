7be3a88cf8988e42812e1ef531996064a587cf03
constROD <rialcantara21@gmail.com>
2023-04-06 05:52:51 +0800

    chore: update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,11 +20,7 @@
 // };
 
 module.exports = {
-  content: [
-    './src/modules/**/*.{js,ts,jsx,tsx}',
-    './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/shared/components/**/*.{js,ts,jsx,tsx}',
-  ],
+  content: ['./src/modules/**/*.{js,ts,jsx,tsx}', './src/pages/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {},
   },
